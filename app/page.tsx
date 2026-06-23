97c58f654775781b2f0924e99c0f8ef5d2e91769
Hykoo13 <haikmonossiandev@gmail.com>
2026-06-23 16:36:24 +0200

    add image and some data

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-neutral-950">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-neutral-950">
-        <section className="flex border-1 w-200 items-center justify-center">
-          <h2>Contact</h2>
+    <main className="flex flex-1 w-full flex-col items-center py-32 px-16 bg-white ">
+      <div className="relative flex w-200 min-h-100 items-start justify-start overflow-hidden rounded-3xl p-16">
+        <form className="flex flex-col z-1">
+          <h2>Contacter l'agence</h2>
+          <div>
+            <fieldset>
+              <legend>Vos informations</legend>
+              <input type="radio" name="Civilité" value="Mr" />
+            </fieldset>
+            <fieldset>
+              <legend>Vos coordonnées</legend>
 
+            </fieldset>
+            <fieldset>
+              <legend>Vos coordonnées</legend>
 
-        </section>
-
-      </main>
-    </div>
+            </fieldset>
+          </div>
+        </form>
+        <Image src="/images/form_background.jpg" fill alt="Une pièce d'appartement" className="object-cover blur-[2px] brightness-50"></Image>
+      </div>
+    </main>
   );
 }
